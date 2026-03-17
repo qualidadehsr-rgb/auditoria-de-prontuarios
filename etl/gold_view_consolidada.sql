a5869b3cf3f170dbdfa1df44262ca2d73dea74e4
ediney-magalhaes <edy.estatistica@gmail.com>
2026-03-17 16:04:10 -0400

    chore: atualiza gitignore e implementa logica 0/1 na view gold

--- etl/gold_view_consolidada.sql
+++ etl/gold_view_consolidada.sql
@@ -17,7 +17,15 @@ SELECT R.id_resposta,
        D.tema_formatado,
        D.pergunta_formatada,
        P.resposta_conformidade,
-       REGEXP_REPLACE(P.observacao_texto, r'[0-9]{4,}', '[CENSURADO]') AS observacao_texto
+       REGEXP_REPLACE(P.observacao_texto, r'[0-9]{4,}', '[CENSURADO]') AS observacao_texto,
+       CASE
+        WHEN P.resposta_conformidade = 'Conforme' THEN 1
+        ELSE 0
+       END AS qtde_conforme,
+       CASE
+        WHEN P.resposta_conformidade IN ('Conforme', 'Não conforme') THEN 1
+        ELSE 0
+       END AS qtde_valida
 FROM `comissao-prontuario.prontuarios_dados.silver_respostas` AS R
 INNER JOIN detalhes_pivot AS P ON R.id_resposta = P.id_resposta
 LEFT JOIN `comissao-prontuario.prontuarios_dados.dim_perguntas` AS D ON P.codigo_base = D.codigo_base
