0d75bfcf664f297b4beee8b3e2e8f5c9c4439c75
ediney-magalhaes <edy.estatistica@gmail.com>
2026-03-18 14:12:31 -0400

    chore: remove script obsoleto de recriacao e backup da silver resposta

--- etl/ddl_silver_respostas.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `comissao-prontuario.prontuarios_dados.respostas`
-PARTITION BY DATE(data_submissao)
-CLUSTER BY nome_empresa, tipo_avaliacao, nome_avaliador, setor_avaliado
-AS
-SELECT * FROM `comissao-prontuario.prontuarios_dados.resposta_bkp`;
